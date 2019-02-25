be79d58750d2a416581d33f4d390ea2dfd481b9f
FengJing <448195466@qq.com>
2019-02-25 17:30:01 +0800

    2.25

--- lesson/build/webpack.prod.config.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-const BaseConfig = require('./webpack.base.config')
-
-const prodConfig = {
-  mode: 'production',
-  devtool: 'cheap-module-source-map',
-}
-
-module.exports = merge(BaseConfig, prodConfig)
